6eefee65e678a077d8ca0e79e8a70ff7a464dc46
Chandan8585 <chandanwingshr@gmail.com>
2023-06-11 23:05:24 +0530

    backend

--- array/Mern-practice/calc.js
+++ array/Mern-practice/calc.js
@@ -1,3 +1,7 @@
-var calc = require('./doc2.js')
-result = calc.add(67,76)
-console.log("output", + result)
+// var calc = require('./doc2.js')
+// result = calc.add(67,76)
+// console.log("output", + result)
+
+console.log(x)
+let x =10;
+// console.log(x)//
